9ded6b300833e3afe922a17ce488c705b83c59d4
James Volmert <james.volmert@gmail.com>
2025-02-01 21:58:27 -0700

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import Pdf from "../assets/James_Resume.pdf";
 
 const Footer = ({ setIsHovered, setGitIsHovered, setResumeIsHovered }) => {
   return (
-    <div className="max-w-full grid  md:grid-cols-3 ">
+    <div className="max-w-full grid md:grid-cols-3 ">
       <div
-        className="border-[4px] border-r-[2px] border-black  flex items-center justify-center"
+        className="md:border-[4px] md:border-r-[2px] sm:border-b-[2px] border-black  flex items-center justify-center"
         id="linkedin"
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
@@ -18,23 +18,23 @@ const Footer = ({ setIsHovered, setGitIsHovered, setResumeIsHovered }) => {
         </a>
       </div>
       <div
-        className="border-[4px] border-r-[2px] border-black  flex items-center justify-center"
+        className="md:border-[4px] sm:border-[4px] md:border-r-[2px] sm:border-b-[2px] sm:border-t-[2px] border-black  flex items-center justify-center"
         id="github"
         onMouseOver={() => setGitIsHovered(true)}
         onMouseOut={() => setGitIsHovered(false)}
       >
         <a href="https://github.com/jamesv28">
-          <h2 className="text-4xl">Github</h2>
+          <h2 className="text-4xl mt-5 mb-5">Github</h2>
         </a>
       </div>
       <div
-        className="border-[4px] border-r-[2px] border-black  flex items-center justify-center"
+        className="md:border-[4px] border-r-[2px] sm:border-b-[2px] sm:border-t-[2px] sm:border-black md:border-black  flex items-center justify-center"
         id="resume"
         onMouseOver={() => setResumeIsHovered(true)}
         onMouseOut={() => setResumeIsHovered(false)}
       >
         <a href={Pdf} target="_blank" rel="noopener noreferrer">
-          <h2 className="text-4xl">Resume</h2>
+          <h2 className="text-4xl mt-5 mb-5">Resume</h2>
         </a>
       </div>
     </div>
